6b1dbd7f4b5f3da01270038c7629a3a987edf3e3
Abhishek Sah <abhi.sah.97@gmail.com>
2019-05-26 12:52:44 +0530

    Adds play method

--- src/GuessTheMovieGame.java
+++ src/GuessTheMovieGame.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class GuessTheMovieGame {
     private String movieName;
     private Integer pointsLost;
@@ -10,8 +12,37 @@ public class GuessTheMovieGame {
         pointsLost = 0;
         guessed = "";
         for (Integer i=0; i<movieName.length(); i++){
-            guessed += "_";
+            if(movieName.charAt(i)!=' ') {
+                guessed += "_";
+            }
+            else{
+                guessed += ' ';
+            }
         }
         System.out.println(guessed.length());
+    }
+    public void play(){
+        while(pointsLost<10 && !guessed.equals(movieName)){
+            System.out.println("You are guessing: " + guessed);
+            System.out.println("You have guessed ("+(pointsLost)+") wrong letters");
+            System.out.println("Guess a letter:");
+            Scanner reader = new Scanner(System.in);
+            char input = reader.next(".").charAt(0);
+            Boolean found = false;
+            for (Integer i=0; i<guessed.length(); i++){
+                if(movieName.charAt(i)==input) {
+                    guessed = guessed.substring(0, i)+movieName.charAt(i)+guessed.substring(i+1);
+                    found = true;
+                }
+            }
+            if(!found) {
+                pointsLost++;
+            }
+        }
+        if(pointsLost>=10){
+            System.out.println("You lost the game!");
+        }else {
+            System.out.println("Congratulations, You Won!");
+        }
     }
 }
