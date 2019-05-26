c80364384cfadbbf68314721652d645b7bb9c9e8
Abhishek Sah <abhi.sah.97@gmail.com>
2019-05-26 12:55:02 +0530

    Starts game play from main method

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Guess the movie game");
+        GuessTheMovieGame guessTheMovieGame = new GuessTheMovieGame(System.getProperty("user.dir") + "/src/movies.txt");
+        System.out.println(System.getProperty("user.dir") + "/src/movies.txt");
+        guessTheMovieGame.play();
     }
 }
