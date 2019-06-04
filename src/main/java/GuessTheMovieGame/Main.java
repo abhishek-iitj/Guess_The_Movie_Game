177528c96720842974cd8552aef95365cfadf3e4
Abhishek Sah <abhi.sah.97@gmail.com>
2019-06-04 11:05:20 +0530

    Fixes file not found error

--- src/main/java/GuessTheMovieGame/Main.java
+++ src/main/java/GuessTheMovieGame/Main.java
@@ -3,7 +3,7 @@ package GuessTheMovieGame;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Guess the movie game");
-        GuessTheMovieGame guessTheMovieGame = new GuessTheMovieGame(System.getProperty("user.dir") + "/src/movies.txt");
+        GuessTheMovieGame guessTheMovieGame = new GuessTheMovieGame(System.getProperty("user.dir") + "/src/main/java/movies.txt");
         guessTheMovieGame.play();
     }
 }
