70dcc699f6dca3c3e93eef05bf2b55e075b384b9
Abhishek Sah <abhi.sah.97@gmail.com>
2019-06-05 21:08:34 +0530

    Updates pom.xml to use surefire plugin, refactors

--- src/test/java/GuessTheMovieGame/PickRandomMovieTest.java
+++ src/test/java/GuessTheMovieGame/PickRandomMovieTest.java
@@ -1,8 +1,10 @@
+package test.java.GuessTheMovieGame;
+
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class PickRandomMovieTest {
+public class PickRandomMovieTest {
     @Test
     void dummyTest() {
         assertEquals(5, 5);
